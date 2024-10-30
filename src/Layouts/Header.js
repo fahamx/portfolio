136b384ae312db779ee19a312da931749bd377e3
faham1997 <nafidfaham08@gmail.com>
2024-10-30 01:10:52 -0230

    update header with link

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Experience from "../Pages/Experience";
+import Photos from "../Pages/Photos";
 
 export default function Header() {
   return (
     <div>
       <header className=" flex justify-between items-center mb-8">
-        <div className="text-fg text-xl font-semibold cursor-pointer">
+        <Link to="/" className="text-fg text-xl font-semibold cursor-pointer">
           faham.dev
-        </div>
+        </Link>
         <nav className=" text-fg  hidden text-lg font-semibold  sm:flex space-x-4">
-          <a href="/experience">experience</a>
-          <a href="/photos">photos</a>
+          <Link to="/experience" className="hover:text-gray-400">
+            experience
+          </Link>
+          <Link to="/photos" className="hover:text-gray-400">
+            photos
+          </Link>
           <a
+            className="hover:text-gray-400"
             href="https://github.com/fahamx"
             target="_blank"
             rel="noopener noreferrer"
