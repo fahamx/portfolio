a1d905ef7b41ebf9afe32b715d0061070763bea4
faham1997 <nafidfaham08@gmail.com>
2024-10-29 22:44:14 -0230

    install tailwind css   and cleanup unessential stuffs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800">
+        Hello, React + Tailwind!
+      </h1>
     </div>
   );
 }
