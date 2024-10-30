82a55c64a9134fb49dbbd013e18c8bf420751f2b
faham1997 <nafidfaham08@gmail.com>
2024-10-30 01:11:19 -0230

    move the Header to main App file

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,10 @@
 import React, { useEffect } from "react";
-import Header from "../Layouts/Header";
 import Profile from "../Components/Profile";
 
-function Home() {
+export default function Home() {
   useEffect(() => {
     document.title = "faham.dev //home";
   }, []);
 
-  return (
-    <div className="  font-mono p-6 bg-black min-h-screen">
-      <Header />
-      <Profile />
-    </div>
-  );
+  return <Profile />;
 }
-
-export default Home;
