6b0fe1e654727031a53f385eef88188b0db23d7d
faham1997 <nafidfaham08@gmail.com>
2024-10-30 01:10:36 -0230

    install react router and add Link for other routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import Home from "./Pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Header from "./Layouts/Header";
+import Home from "./Pages/Home";
+import Experience from "./Pages/Experience";
+import Photos from "./Pages/Photos";
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div className="  font-mono p-6 bg-black min-h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/experience" element={Experience} />
+          <Route path="/photos" element={Photos} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
