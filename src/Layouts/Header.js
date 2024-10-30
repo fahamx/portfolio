995c6108cfb7c748343e0e69e45b36d0137af190
faham1997 <nafidfaham08@gmail.com>
2024-10-29 23:27:17 -0230

    redo the header

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -3,26 +3,15 @@ import React from "react";
 export default function Header() {
   return (
     <div>
-      <header className="flex items-center justify-between p-4 bg-black overflow-hidden ">
-        <div className="text-white font-semibold cursor-pointer">faham.dev</div>
-
-        <nav className="flex space-x-4">
-          <a
-            href="/experience"
-            className="text-white font-semibold hover:text-blue-600"
-          >
-            experience
-          </a>
-          <a
-            href="/photos"
-            className="text-white font-semibold hover:text-blue-600"
-          >
-            photos
-          </a>
+      <header className="flex justify-between items-center mb-8">
+        <div className="text-xl font-bold cursor-pointer">faham.dev</div>
+        <nav className="hidden text-lg font-bold sm:flex space-x-4">
+          <a href="/experience">experience</a>
+          <a href="/photos">photos</a>
           <a
             href="https://github.com/fahamx"
             target="_blank"
-            className="text-white font-semibold hover:text-blue-600"
+            rel="noopener noreferrer"
           >
             github
           </a>
